bd259adaad55d247a0da95f14570851e9ac93bd0
John Freeman <jpf32005@gmail.com>
2026-05-22 21:27:59 -0700

    arrow spin

--- arrowProj.gd
+++ arrowProj.gd
@@ -1,9 +1,14 @@
 extends Projectile
 
+@export var spin = .1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	
+func _physics_process(delta: float) -> void:
+	if move:
+		position += direction * speed * delta
+		rotation += spin
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
