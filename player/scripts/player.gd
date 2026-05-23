215bc8f41440f1cdeeec23fba47ca05cac496755
entityJY <jomathya07@gmail.com>
2026-05-22 20:12:00 -0700

    Edit keymap

---
+++ player/scripts/player.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name Player
+
+@export var move_speed: float
+@export var dash_speed: float
+
+func _ready() -> void:
+    pass
+
+func _physics_process(delta: float) -> void:
+    pass
