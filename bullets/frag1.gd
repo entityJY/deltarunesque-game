3884d0e05b3b2b18c5ea51aa4210903998c6eb3a
Andrea Ng <andreang0323@gmail.com>
2026-05-22 21:26:54 -0700

    added frag bullet base

---
+++ bullets/frag1.gd
@@ -0,0 +1,30 @@
+extends Projectile
+
+@export var timeout_duration : float
+@export var fragment_count : int
+@export var freeze_duration : float
+@export var child_scene : String = "res://bullets/base_bullet.tscn"
+
+func initBullet() -> void:
+	var childInstance = load(child_scene)
+	# Wait before splitting
+	await get_tree().create_timer(timeout_duration).timeout
+	
+	move = false
+	
+	await get_tree().create_timer(freeze_duration).timeout
+	
+	
+	var childBullets = []
+	# initialize all bullets
+	for f in range(fragment_count):
+		var newBullet = childInstance.instantiate()
+		childBullets.append(newBullet)
+		newBullet.direction = direction.rotated(deg_to_rad(360.0/fragment_count * f))
+	
+	# add all bullets
+	for childFragment in childBullets:
+		childFragment.global_position = global_position
+		get_parent().add_child(childFragment)
+
+	disable()
